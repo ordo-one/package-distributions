ac7c5d3f2ae70021124a5bac603527c0f279c273
Dianna Ma <mdianna1989@gmail.com>
2025-06-27 23:59:05 +0000

    initial commit

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version:6.1
+import PackageDescription
+
+let package:Package = .init(
+    name: "package-distributions",
+    products: [
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-numerics", from: "1.0.3"),
+    ],
+    targets: [
+        .target(
+            name: "Random",
+            dependencies: [
+                .product(name: "RealModule", package: "swift-numerics"),
+            ],
+            linkerSettings: [
+                .linkedLibrary("m"),
+            ],
+        ),
+
+        .testTarget(
+            name: "RandomTests",
+            dependencies: [
+                .target(name: "Random"),
+            ],
+        ),
+    ]
+)
