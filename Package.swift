3e6cf93f5055836bc91a77e0da266b382f40da0b
Diana Ma <diana@rarestype.com>
2025-08-09 23:46:55 +0000

    wrong xcode scheme name

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:6.1
 import PackageDescription
 
-let package:Package = .init(
+let package: Package = .init(
     name: "package-distributions",
     platforms: [.macOS(.v15), .iOS(.v18), .tvOS(.v18), .visionOS(.v2), .watchOS(.v11)],
     products: [
