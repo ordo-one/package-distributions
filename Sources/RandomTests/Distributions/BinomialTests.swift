83a123d987568188e71f444a185581c49a0881c7
Diana Ma <diana@rarestype.com>
2026-02-25 02:44:25 +0000

    calibrate

--- Sources/RandomTests/Distributions/BinomialTests.swift
+++ Sources/RandomTests/Distributions/BinomialTests.swift
@@ -43,7 +43,7 @@ extension BinomialTests {
         Binomial[n, p].performStandardStatisticalTests(
             sampleCount: 1_000_000,
             using: &self.random,
-            visualize: n < 1_000_000
+            visualize: Double.init(n) * p < 10_000
         )
     }
     @Test(
@@ -59,7 +59,7 @@ extension BinomialTests {
         Binomial[n, p].performStandardStatisticalTests(
             sampleCount: 1_000_000,
             using: &self.random,
-            visualize: n < 1_000_000
+            visualize: Double.init(n) * p < 10_000
         )
     }
 
